237ea9d05a2a73afd38f74be92b8ed3dc33c64da
Erlin Emrys <Erlin@seznam.cz>
2025-06-26 12:24:29 +0100

    Fixing issues

--- src/Index.ts
+++ src/Index.ts
@@ -1,7 +1,7 @@
 // noinspection JSUnusedGlobalSymbols
 
-export type { ConsoleLogger } from "./Logging/ConsoleLogger";
+export { ConsoleLogger } from "./Logging/ConsoleLogger";
 export type { ILog, ILogSink } from "./Logging/Log";
 export { Log } from "./Logging/Log";
-export type { LogSink } from "./Logging/LogSink";
+export { LogSink } from "./Logging/LogSink";
 export default Log;
