f90a86b0ef26b94d1fadc6a82fe488abb59dc940
Erlin Emrys <Erlin@seznam.cz>
2025-07-05 23:28:52 +0100

    Better logging

--- src/Index.ts
+++ src/Index.ts
@@ -2,6 +2,6 @@
 
 export { ConsoleLogger } from "./Logging/ConsoleLogger";
 export type { ILog, ILogSink } from "./Logging/Log";
-export { Log } from "./Logging/Log";
+export { Log, LogLevel } from "./Logging/Log";
 export { LogSink } from "./Logging/LogSink";
 export default Log;
